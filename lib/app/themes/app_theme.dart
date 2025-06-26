7c1b6d64c3567c961c9c030d5ba7f3e60eda9a9d
johnson-oragui <johnson.oragui@gmail.com>
2025-06-26 12:04:49 +0100

    feat: added app_theme

---
+++ lib/app/themes/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primarySwatch: Colors.deepPurple,
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: 'Poppins',
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
